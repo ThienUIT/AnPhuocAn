588b191961ab02ab556f2e9b8f25a117c92205c7
Thien, Tran Dinh <17521080@gm.uit.edu.vn>
2022-09-22 13:18:10 +0700

    create app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from './view/HomePage';
+
+function App() {
+	return (
+		<div className='App'>
+			<HomePage></HomePage>
+		</div>
+	);
+}
+
+export default App;
